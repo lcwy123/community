53d9bad9513396283a138ffc96b363c127545884
gzh <923905645@qq.com>
2022-08-27 16:05:29 +0800

    community0827

--- src/main/java/com/nowcoder/community/controller/MessageController.java
+++ src/main/java/com/nowcoder/community/controller/MessageController.java
@@ -138,4 +138,12 @@ public class MessageController {
         }
         return ids;
     }
+
+    // 删除私信
+    @RequestMapping(path = "/letter/delete", method = RequestMethod.POST)
+    @ResponseBody
+    public String deleteLetter(int id) {
+        messageService.deleteMessage(id);
+        return CommunityUtil.getJSONString(0);
+    }
 }
