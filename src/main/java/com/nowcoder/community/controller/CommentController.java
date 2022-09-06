3c127957184bf8414586edd30b789ed6ed3c116c
gzh <923905645@qq.com>
2022-09-06 15:48:06 +0800

    community0906

--- src/main/java/com/nowcoder/community/controller/CommentController.java
+++ src/main/java/com/nowcoder/community/controller/CommentController.java
@@ -1,7 +1,11 @@
 package com.nowcoder.community.controller;
 
 import com.nowcoder.community.entity.Comment;
+import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.entity.Event;
+import com.nowcoder.community.event.EventProducer;
 import com.nowcoder.community.service.CommentService;
+import com.nowcoder.community.service.DiscussPostService;
 import com.nowcoder.community.util.CommunityConstant;
 import com.nowcoder.community.util.HostHolder;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +26,12 @@ public class CommentController implements CommunityConstant {
     @Autowired
     private CommentService commentService;
 
+    @Autowired
+    private EventProducer eventProducer;
+
+    @Autowired
+    private DiscussPostService discussPostService;
+
     @RequestMapping(path = "/add/{discussPostId}", method = RequestMethod.POST)
     public String addComment(@PathVariable("discussPostId") int discussPostId, Comment comment) {
 
@@ -31,7 +41,7 @@ public class CommentController implements CommunityConstant {
         commentService.addComment(comment);
 
         // 触发评论事件
-        /*Event event = new Event()
+        Event event = new Event()
                 .setTopic(TOPIC_COMMENT)
                 .setUserId(hostHolder.getUser().getId())
                 .setEntityType(comment.getEntityType())
@@ -44,7 +54,7 @@ public class CommentController implements CommunityConstant {
             Comment target = commentService.findCommentById(comment.getEntityId());
             event.setEntityUserId(target.getUserId());
         }
-        eventProducer.fireEvent(event);*/
+        eventProducer.fireEvent(event);
 
         return "redirect:/discuss/detail/" + discussPostId;
 
--- src/main/java/com/nowcoder/community/controller/LikeController.java
+++ src/main/java/com/nowcoder/community/controller/LikeController.java
@@ -1,6 +1,8 @@
 package com.nowcoder.community.controller;
 
+import com.nowcoder.community.entity.Event;
 import com.nowcoder.community.entity.User;
+import com.nowcoder.community.event.EventProducer;
 import com.nowcoder.community.service.LikeService;
 import com.nowcoder.community.util.CommunityConstant;
 import com.nowcoder.community.util.CommunityUtil;
@@ -23,10 +25,12 @@ public class LikeController implements CommunityConstant {
     @Autowired
     private HostHolder hostHolder;
 
+    @Autowired
+    private EventProducer eventProducer;
 
     @RequestMapping(path = "/like", method = RequestMethod.POST)
     @ResponseBody
-    public String like(int entityType, int entityId, int entityUserId) {
+    public String like(int entityType, int entityId, int entityUserId, int postId) {
         User user = hostHolder.getUser();
 
         // 点赞
@@ -40,7 +44,7 @@ public class LikeController implements CommunityConstant {
         map.put("likeCount", likeCount);
         map.put("likeStatus", likeStatus);
 
-/*        // 触发点赞事件
+        // 触发点赞事件
         if(likeStatus == 1) {
             Event event = new Event()
                     .setTopic(TOPIC_LIKE)
@@ -50,7 +54,7 @@ public class LikeController implements CommunityConstant {
                     .setEntityUserId(entityUserId)
                     .setData("postId", postId);
             eventProducer.fireEvent(event);
-        }*/
+        }
 
         return CommunityUtil.getJSONString(0, null, map);
     }
